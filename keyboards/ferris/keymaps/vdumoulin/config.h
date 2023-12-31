fb1914535682efb5932a7ea31d93dbef492e64a5
Vincent Dumoulin <vi.dumoulin@gmail.com>
2023-12-30 22:19:17 -0500

    Update keymap

--- keyboards/ferris/keymaps/vdumoulin/config.h
+++ keyboards/ferris/keymaps/vdumoulin/config.h
@@ -2,7 +2,6 @@
 
 #define FORCE_NKRO
 
-// Values copied from the default keymap.
 #define MOUSEKEY_DELAY 100
 #define MOUSEKEY_INTERVAL 16
 #define MOUSEKEY_TIME_TO_MAX 40
@@ -10,6 +9,5 @@
 #define MOUSEKEY_WHEEL_INTERVAL 50
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 100
 
-// Keep the auto-repeat ability by *not* setting QUICK_TAP_TERM to 0.
 #define PERMISSIVE_HOLD
 #define TAPPING_TERM 200
--- keyboards/ferris/keymaps/vdumoulin/keymap.c
+++ keyboards/ferris/keymaps/vdumoulin/keymap.c
@@ -1,15 +1,4 @@
 // Vincent Dumoulin's Ferris Sweep keyboard layout.
-//
-// Reuses Miryoku (QWERTY, vi navigation) layout conventions with a few
-// simplifications and tweaks:
-//
-//   * No Button layer and no Extra and Tap alternate base layers.
-//   * Double-tap buttons are modified to have the following functionalities
-//     (index to pinkie, {left,right} hand): DM_PLY{1,2} (single-tap), DM_RSTP
-//     (single-tap), DM_REC{1,2} (single-tap), QK_REBOOT QK_BOOT.
-//   * No layer-lock additional functionality.
-//   * No RGB or bluetooth functionalities in the Media layer; the index and
-//     middle fingers in the top row control screen brightness instead.
 
 #include QMK_KEYBOARD_H
 
@@ -21,12 +10,14 @@ enum layers {
 };
 
 /* Home row mods */
-#define KC_GUIA LGUI_T(KC_A)
-#define KC_ALTS LALT_T(KC_S)
-#define KC_CTLD LCTL_T(KC_D)
-#define KC_CTLK LCTL_T(KC_K)
-#define KC_ALTL LALT_T(KC_L)
-#define KC_GUOT LGUI_T(KC_QUOT)
+#define _G_A LGUI_T(KC_A)
+#define _A_S LALT_T(KC_S)
+#define _C_D LCTL_T(KC_D)
+#define _S_F LSFT_T(KC_F)
+#define _S_J LSFT_T(KC_J)
+#define _C_K LCTL_T(KC_K)
+#define _A_L LALT_T(KC_L)
+#define _G_QUOT LGUI_T(KC_QUOT)
 
 /* One-shot mods */
 #define OS_LSFT OSM(MOD_LSFT)
@@ -35,8 +26,8 @@ enum layers {
 #define OS_LGUI OSM(MOD_LGUI)
 
 /* Layer toggling */
-#define LA_SYM MO(_SYM)
-#define LA_NUM MO(_NUM)
+#define OS_SYM OSL(_SYM)
+#define OS_NUM OSL(_NUM)
 
 /* Tap-dance */
 enum tap_dance {
@@ -67,42 +58,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //┌─────────┬─────────┬─────────┬─────────┬─────────┐    ┌─────────┬─────────┬─────────┬─────────┬─────────┐
         KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,      KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    ,
     //├─────────┼─────────┼─────────┼─────────┼─────────┤    ├─────────┼─────────┼─────────┼─────────┼─────────┤
-        KC_GUIA , KC_ALTS , KC_CTLD , KC_F    , KC_G    ,      KC_H    , KC_J    , KC_CTLK , KC_ALTL , KC_GUOT ,
+        _G_A    , _A_S    , _C_D    , _S_F    , KC_G    ,      KC_H    , _S_J    , _C_K    , _A_L    , _G_QUOT ,
     //├─────────┼─────────┼─────────┼─────────┼─────────┤    ├─────────┼─────────┼─────────┼─────────┼─────────┤
         KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    ,      KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH ,
     //└─────────┴─────────┴─────────┼─────────┼─────────┤    ├─────────┼─────────┴─────────┴─────────┴─────────┘
-                                      LA_NUM  , KC_SPC  ,      KC_LSFT , LA_SYM
+                                      OS_NUM  , KC_SPC  ,      OS_LSFT , OS_SYM
     //                              └─────────┴─────────┘    └─────────┴─────────┘
     ),
     [_SYM] = LAYOUT_split_3x5_2(
     //┌─────────┬─────────┬─────────┬─────────┬─────────┐    ┌─────────┬─────────┬─────────┬─────────┬─────────┐
-        KC_LPRN , KC_LBRC , KC_LCBR , KC_LABK , OS_LCTL ,      OS_LSFT , KC_RABK , KC_RCBR , KC_RBRC , KC_RPRN ,
+        KC_LPRN , KC_LBRC , KC_LCBR , KC_LABK , OS_LGUI ,      OS_LCTL , KC_RABK , KC_RCBR , KC_RBRC , KC_RPRN ,
     //├─────────┼─────────┼─────────┼─────────┼─────────┤    ├─────────┼─────────┼─────────┼─────────┼─────────┤
         KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC ,      KC_CIRC , KC_AMPR , KC_ASTR , KC_BSLS , KC_SCLN ,
     //├─────────┼─────────┼─────────┼─────────┼─────────┤    ├─────────┼─────────┼─────────┼─────────┼─────────┤
-        KC_GRV  , KC_TILD , KC_PLUS , KC_UNDS , OS_LGUI ,      OS_LALT , KC_MINS , KC_EQL  , KC_PIPE , KC_COLN ,
+        KC_GRV  , KC_TILD , KC_PLUS , KC_UNDS , OS_LSFT ,      OS_LALT , KC_MINS , KC_EQL  , KC_PIPE , KC_COLN ,
     //└─────────┴─────────┴─────────┼─────────┼─────────┤    ├─────────┼─────────┴─────────┴─────────┴─────────┘
                                       _______ , _______ ,      _______ , _______
     //                              └─────────┴─────────┘    └─────────┴─────────┘
     ),
     [_NUM] = LAYOUT_split_3x5_2(
     //┌─────────┬─────────┬─────────┬─────────┬─────────┐    ┌─────────┬─────────┬─────────┬─────────┬─────────┐
-        KC_ESC  , KC_F7   , KC_F8   , KC_F9   , OS_LCTL ,      OS_LSFT , KC_7    , KC_8    , KC_9    , KC_BSPC ,
+        KC_ESC  , KC_F7   , KC_F8   , KC_F9   , OS_LGUI ,      OS_LCTL , KC_7    , KC_8    , KC_9    , KC_BSPC ,
     //├─────────┼─────────┼─────────┼─────────┼─────────┤    ├─────────┼─────────┼─────────┼─────────┼─────────┤
         KC_TAB  , KC_F4   , KC_F5   , KC_F6   , KC_F10  ,      KC_0    , KC_4    , KC_5    , KC_6    , KC_ENT  ,
     //├─────────┼─────────┼─────────┼─────────┼─────────┤    ├─────────┼─────────┼─────────┼─────────┼─────────┤
-        CW_TOGG , KC_F1   , KC_F2   , KC_F3   , OS_LGUI ,      OS_LALT , KC_1    , KC_2    , KC_3    , KC_DEL  ,
+        CW_TOGG , KC_F1   , KC_F2   , KC_F3   , OS_LSFT ,      OS_LALT , KC_1    , KC_2    , KC_3    , KC_DEL  ,
     //└─────────┴─────────┴─────────┼─────────┼─────────┤    ├─────────┼─────────┴─────────┴─────────┴─────────┘
                                       _______ , _______ ,      _______ , _______
     //                              └─────────┴─────────┘    └─────────┴─────────┘
     ),
     [_EXT] = LAYOUT_split_3x5_2(
     //┌─────────┬─────────┬─────────┬─────────┬─────────┐    ┌─────────┬─────────┬─────────┬─────────┬─────────┐
-        KC_BRID , KC_BRIU , KC_VOLD , KC_VOLU , KC_MUTE ,      DM_RSTP , DM_PLY1 , DM_PLY2 , DM_REC1 , DM_REC2 ,
+        KC_VOLD , KC_VOLU , KC_MUTE , KC_F11  , KC_F12  ,      KC_MS_L , KC_MS_D , KC_MS_U , KC_MS_R , KC_BRIU ,
     //├─────────┼─────────┼─────────┼─────────┼─────────┤    ├─────────┼─────────┼─────────┼─────────┼─────────┤
-        OS_LGUI , OS_LALT , OS_LCTL , OS_LSFT , KC_F12  ,      KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , TD(TBO) ,
+        DM_RSTP , DM_PLY1 , DM_PLY2 , DM_REC1 , DM_REC2 ,      KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_BRID ,
     //├─────────┼─────────┼─────────┼─────────┼─────────┤    ├─────────┼─────────┼─────────┼─────────┼─────────┤
-        KC_MSTP , KC_MPRV , KC_MNXT , KC_MPLY , KC_F11  ,      KC_HOME , KC_PGDN , KC_PGUP , KC_END  , TD(TRE) ,
+        TD(TRE) , KC_MPRV , KC_MNXT , KC_MSTP , KC_MPLY ,      KC_HOME , KC_PGDN , KC_PGUP , KC_END  , TD(TBO) ,
     //└─────────┴─────────┴─────────┼─────────┼─────────┤    ├─────────┼─────────┴─────────┴─────────┴─────────┘
                                       _______ , _______ ,      _______ , _______
     //                              └─────────┴─────────┘    └─────────┴─────────┘
